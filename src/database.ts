6b20683b0f9d420a1fff70607a3cb584ad6c8bd5
ptrkc <40273827+ptrkc@users.noreply.github.com>
2021-08-01 05:22:49 -0300

    Adding log to debug deploy errors

--- src/database.ts
+++ src/database.ts
@@ -8,18 +8,23 @@ if (
     process.env.DATABASE_URL += "?sslmode=require";
 }
 export default async function connect() {
-    const connectionManager = await getConnectionManager();
-    const connection = connectionManager.create({
-        name: "default",
-        type: "postgres",
-        url: process.env.DATABASE_URL,
-        entities: [
-            `${
-                process.env.NODE_ENV === "production" ? "dist/src" : "src"
-            }/entities/*.*`,
-        ],
-        ssl: process.env.NODE_ENV === "production",
-    });
-    await connection.connect();
-    return connection;
+    try {
+        const connectionManager = getConnectionManager();
+        const connection = connectionManager.create({
+            name: "default",
+            type: "postgres",
+            url: process.env.DATABASE_URL,
+            entities: [
+                `${
+                    process.env.NODE_ENV === "production" ? "dist/src" : "src"
+                }/entities/*.*`,
+            ],
+            ssl: process.env.NODE_ENV === "production",
+        });
+        await connection.connect();
+        await connection.runMigrations();
+        return connection;
+    } catch (e) {
+        console.log(e);
+    }
 }
