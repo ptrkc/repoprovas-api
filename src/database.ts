aa35fc3cd04387542bd13582756c635b533002b4
ptrkc <40273827+ptrkc@users.noreply.github.com>
2021-08-01 07:00:25 -0300

    And another

--- src/database.ts
+++ src/database.ts
@@ -1,5 +1,12 @@
 import { getConnectionManager } from "typeorm";
 
+if (
+    process.env.NODE_ENV === "production" &&
+    process.env.DATABASE_URL.indexOf("sslmode=require") === -1
+) {
+    process.env.DATABASE_URL += "?sslmode=require";
+}
+
 export default async function connect() {
     const connectionManager = await getConnectionManager();
     const connection = connectionManager.create({
@@ -11,10 +18,12 @@ export default async function connect() {
                 process.env.NODE_ENV === "production" ? "dist" : "src"
             }/entities/*.*`,
         ],
-        ssl:
-            process.env.NODE_ENV === "production"
-                ? { rejectUnauthorized: false }
-                : false,
+        extra: {
+            ssl:
+                process.env.NODE_ENV === "production"
+                    ? { rejectUnauthorized: false }
+                    : false,
+        },
     });
     await connection.connect();
     return connection;
